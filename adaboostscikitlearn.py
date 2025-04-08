0f284dc0321b946e22501cc2a6aeeb4b1d10629e
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-04-08 22:07:39 +0200

    showing the difference with the decisiontree classifier

--- adaboostscikitlearn.py
+++ adaboostscikitlearn.py
@@ -181,7 +181,7 @@ df_wine_data = """1,14.23,1.71,2.43,15.6,127,2.8,3.06,.28,2.29,5.64,1.04,3.92,10
 
 from io import StringIO
 
-
+import numpy as np
 
 df_wine = pd.read_csv(StringIO(df_wine_data), header=None)
 print(df_wine.head())
@@ -216,3 +216,29 @@ y_test_pred = ada.predict(X_test)
 ada_train = accuracy_score(y_train, y_train_pred)
 ada_test = accuracy_score(y_test, y_test_pred)
 print(f'AdaBoost train/test accuracies {ada_train:.3f}/{ada_test:.3f}')
+
+# check what the decision regions look like:
+x_min = X_train[:, 0].min() - 1
+x_max = X_train[:, 0].max() + 1
+y_min = X_train[:, 1].min() - 1
+y_max = X_train[:, 1].max() + 1
+
+
+import matplotlib.pyplot as plt
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
+f, axarr = plt.subplots(1, 2, sharex = 'col', sharey = 'row', figsize=(8,3))
+for idx, clf, tt in zip([0, 1], [tree, ada], ['Decision tree', 'AdaBoost']):
+    clf.fit(X_train, y_train)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    axarr[idx].contourf(xx, yy, Z, apha=0.3)
+    axarr[idx].scatter(X_train[y_train==0, 0], X_train[y_train==0,1], c = 'blue', marker = '^')
+    axarr[idx].scatter(X_train[y_train==1, 0], X_train[y_train==1, 1], c='green', marker = 'o')
+
+    axarr[idx].set_title(tt)
+    axarr[0].set_ylabel('OD280/OD315 of diluted wines', fontsize=12)
+
+plt.tight_layout()
+plt.text(0, -0.2, s='Alcohol', ha='center', va='center', fontsize=12, transform=axarr[1].transAxes)
+plt.show()
